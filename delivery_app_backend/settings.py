3c0f7ae88de89920e9ef8bf34c2b53d180ee01c6
Jeswin Jose <jeswin4jose@gmail.com>
2025-09-13 18:30:03 +0530

    Initial commit - clean version without secrets

--- delivery_app_backend/settings.py
+++ delivery_app_backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': {
