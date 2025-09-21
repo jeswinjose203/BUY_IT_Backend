2713ea149a731d460f0ebf59e2e9a9bcdc264319
Jeswin Jose <68839902+jeswinjose203@users.noreply.github.com>
2025-09-21 11:24:19 +0530

    Update settings.py

--- delivery_app_backend/settings.py
+++ delivery_app_backend/settings.py
@@ -17,6 +17,8 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 env = environ.Env(
     DEBUG=(bool, False)
 )
